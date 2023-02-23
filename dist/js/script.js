464f6d2794cff775094b48701eb10eca1a9d7571
Vitalii Hrytsenko <vitalik.gricenko77@gmail.com>
2023-02-23 19:55:46 +0200

    add header-slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,176 +1,3 @@
-
-//   //! 1 варіант (простий)
-
-//   // const sliderPrev = document.querySelector('.header-slider__arrow-prev'),
-//   //       sliderNext = document.querySelector('.header-slider__arrow-next'),
-//   //       sliderCurrent = document.querySelector('#current'),
-//   //       sliderTotal = document.querySelector('#total'),
-//   //       sliderItem = document.querySelectorAll('.swiper-slide'),
-//   //       sliderItemText = document.querySelectorAll('.header-text');
-
-
-
-
-//   //! 2 Варіант 
-
-//   const prev = document.querySelector('.header-slider__arrow-prev'),
-//         next = document.querySelector('.header-slider__arrow-next'),
-//         current = document.querySelector('#current'),
-//         total = document.querySelector('#total'),
-//         slides = document.querySelectorAll('.swiper-slide'),
-//         slidesWrapper = document.querySelector('.swiper-wrapper'),
-//         slidesField = document.querySelector(fill),
-//         width = window.getComputedStyle(slidesWrapper).width,
-//         slider = document.querySelector(container);
-
-//   let slideIndex = 1;
-//   let offset = 0;
-
-//   if (slides.length < 10) { // якщо слайдів меньше 10, то підставляємо в тотал з 0
-//     total.textContent = `0${slides.length}`; 
-//     current.textContent = `0${slideIndex}`;
-//   } else { // якщо більше то просто кількість
-//     total.textContent = slides.length;
-//     current.textContent = slideIndex;
-//   }
-
-//   slidesField.style.width = 100 * slides.length + '%'; // щоб карусель займала стіки, скіки для слайдів треба
-//   slidesField.style.display = 'flex';
-//   slidesField.style.transition = '0.5s all';
-
-//   slidesWrapper.style.overflow = 'hidden';
-
-//   slider.style.position = 'relative'; // для точок
-
-//   const indicators = document.createElement('ol'),
-//         dots = []; // всі точки тут
-//   indicators.classList.add('carousel-indicators');
-//   indicators.style.cssText = `
-//     position: absolute;
-//     right: 0;
-//     bottom: 0;
-//     left: 0;
-//     z-index: 15;
-//     display: flex;
-//     justify-content: center;
-//     margin-right: 15%;
-//     margin-left: 15%;
-//     list-style: none;
-//   `;
-
-//   slider.append(indicators); // точки в слайдер
-
-//   function createDots() {
-//     for(let i = 0; i < slides.length; i++) { // создає точок стіки скіки слайдов
-//       const dot = document.createElement('li');
-//       dot.setAttribute('data-slide-to', i + 1);
-//       dot.style.cssText = `
-//         box-sizing: content-box;
-//         flex: 0 1 auto;
-//         width: 30px;
-//         height: 6px;
-//         margin-right: 3px;
-//         margin-left: 3px;
-//         cursor: pointer;
-//         background-color: #fff;
-//         background-clip: padding-box;
-//         border-top: 10px solid transparent;
-//         border-bottom: 10px solid transparent;
-//         opacity: .5;
-//         transition: opacity .6s ease;
-//       `;
-//       if (i == 0) {
-//         dot.style.opacity = 1;
-//       }
-  
-//       indicators.append(dot);
-  
-//       dots.push(dot); // закидую в масив всі точки які зроблені
-//     }
-//   }
-
-//   function calcNextOffset(int) {
-//     if(int) { // int. якщо вперед гортати, то true, а назад false
-//       if (offset == +width.replace(/\D/g, '') * (slides.length - 1)) { // якщо прокрутили до кінця, ширина слайда * на кількість, то певертаємо транслейт на 0, на перший слайд
-//         offset = 0;
-//       } else {
-//         offset += +width.replace(/\D/g, ''); // якщо ще не в кінці, то просто гортаємо на ширину 1 слайда
-//       }
-//     } else {
-//       if (offset == 0) {
-//         offset = +width.replace(/\D/g, '') * (slides.length - 1); // якщо перший слайд, то перекидаємо на останній
-//       } else {
-//         offset -= +width.replace(/\D/g, ''); // якщо це не перший слайд, то віднімаємо ширину 1 слайда
-//       }
-//       //? replace(/\D/g, '') - замінює всі не цифри на нічого, убирає
-//     }
-    
-//   }
-
-//   function calcSlidesIndex() {
-//     if (slides.length < 10) {
-//       current.textContent = `0${slideIndex}`;
-//     } else {
-//       current.textContent = slideIndex;
-//     }
-//   }
-
-//   function dotsToggle() {
-//     dots.forEach(dot => dot.style.opacity = ".5"); // всім точкам прозрачность
-//     dots[slideIndex - 1].style.opacity = 1; // при зміні вибраного слайда давать такій же точкі по счету активность
-//   }
-
-//   function slidesTranslate() {
-//     slidesField.style.transform = `translateX(-${offset}px)`;
-//   }
-//   createDots();
-
-//   slides.forEach(slide => slide.style.width = width); // шоб слайди були однакові та влізли
-
-//   next.addEventListener('click', () => {
-//     calcNextOffset(true);
-//     slidesTranslate(); // свайпає слайди на offset
-
-//     if (slideIndex == slides.length) {
-//       slideIndex = 1;
-//     } else {
-//       slideIndex++;
-//     }
-
-//     calcSlidesIndex();
-//     dotsToggle();
-  
-//   })
-
-//   prev.addEventListener('click', () => {
-
-//     calcNextOffset(false);
-//     slidesTranslate();
-
-//     if (slideIndex == 1) {
-//       slideIndex = slides.length;
-//     } else {
-//       slideIndex--;
-//     }
-
-//     calcSlidesIndex();
-//     dotsToggle();
-//   });
-
-//   dots.forEach(dot => {
-//     dot.addEventListener('click', (e) => {
-//       const slideTo = e.target.getAttribute('data-slide-to'); // та точка на яку клік дає свій номер
-
-//       slideIndex = slideTo; // цей номер клікнутой кидаємо для індекса
-//       offset = +width.slice(0, width.length - 2) * (slideTo - 1);
-
-//       slidesTranslate()
-//       calcSlidesIndex();
-//       dotsToggle();
-//     })
-//   })
-
-
 const swiper = new Swiper('.preference-slider', {
   slidesPerView: 3,
   navigation: {
@@ -347,7 +174,7 @@ function renderItems(data) {
 }
 
 
-
+// плавний скрол
 const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
@@ -362,3 +189,44 @@ for (let anchor of anchors) {
     })
   })
 }
+
+// slider (говнокод якийся вийшов)
+
+let carousel1 = document.querySelector('.header-slider'),
+    carousel2 = document.querySelector('.header-slider-2'),
+    slides = document.querySelectorAll('.header-slide'),
+    current = document.querySelector('#current'),
+    total = document.querySelector('#total'),
+    currentIndex = 0;
+
+function calcCurrentTotal() {
+  if (slides.length < 10) { // якщо слайдів меньше 10, то підставляємо в тотал з 0
+    total.textContent = `0${slides.length / 2}`;  
+    current.textContent = `0${currentIndex + 1}`;
+  } else { // якщо більше то просто кількість
+    total.textContent = slides.length / 2;
+    current.textContent = currentIndex + 1;
+  }
+}
+calcCurrentTotal();
+
+function goToSlide(slideIndex) {
+  carousel1.style.transform = `translateX(-${slideIndex * 100}vw)`;
+  carousel2.style.transform = `translateX(-${slideIndex * 100}vw)`;
+}
+
+document.querySelector('.header-slider__arrow-prev').addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    goToSlide(currentIndex);
+  }
+  calcCurrentTotal();
+});
+
+document.querySelector('.header-slider__arrow-next').addEventListener('click', () => {
+  if (currentIndex < carousel1.children.length - 1) {
+    currentIndex++;
+    goToSlide(currentIndex);
+  }
+  calcCurrentTotal();
+});
